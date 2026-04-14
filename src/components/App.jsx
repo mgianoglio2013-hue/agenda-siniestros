c29ae563c7694f521ace31c13ab14b5710aa5d9e
mgianoglio2013-hue <mgianoglio2013@gmail.com>
2026-04-14 18:00:16 -0300

    Refactor App component to include ChatAgente

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,38 @@
 import { useState } from 'react'
-import AgendaSiniestros from './components/AgendaSiniestros.jsx'
-import DictamenIA from './components/DictamenIA.jsx'
+import AgendaSiniestros from './AgendaSiniestros.jsx'
+import DictamenIA from './DictamenIA.jsx'
+import ChatAgente from './ChatAgente.jsx'
 
 function App() {
-  const [modulo, setModulo] = useState('agenda')
-  const [siniestroSeleccionado, setSiniestroSeleccionado] = useState(null)
+    const [modulo, setModulo] = useState('agenda')
+    const [siniestroSeleccionado, setSiniestroSeleccionado] = useState(null)
+    const [siniestros, setSiniestros] = useState([])
+    const [chatOpen, setChatOpen] = useState(false)
 
   return (
-    <div>
-      {modulo === 'agenda' && (
-        <AgendaSiniestros 
-          onAbrirDictamen={(siniestro) => {
-            setSiniestroSeleccionado(siniestro)
-            setModulo('dictamen')
-          }}
-        />
-      )}
-      {modulo === 'dictamen' && (
-        <DictamenIA 
-          siniestro={siniestroSeleccionado}
-          onVolver={() => setModulo('agenda')}
-        />
-      )}
-    </div>
-  )
+        <div>
+          {modulo === 'agenda' && (
+                  <AgendaSiniestros 
+                              onAbrirDictamen={(siniestro) => {
+                                            setSiniestroSeleccionado(siniestro)
+                                                          setModulo('dictamen')
+                              }}
+                              onSiniestrosCargados={setSiniestros}
+                            />
+                )}
+          {modulo === 'dictamen' && (
+                  <DictamenIA 
+                              siniestro={siniestroSeleccionado}
+                              onVolver={() => setModulo('agenda')}
+                            />
+                )}
+              <ChatAgente 
+                        siniestros={siniestros}
+                        isOpen={chatOpen}
+                        onToggle={() => setChatOpen(!chatOpen)}
+                      />
+        </div>div>
+      )
 }
 
-export default App
+export default App</div>
